f3df7bac7c5e554ba8f7f1ed95e6b8962053ef1d
Spirit-Tiger <sprttiger@gmail.com>
2022-08-26 15:36:36 +0300

    package.json update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
     app.use(graphqlUploadExpress());
 
     server.applyMiddleware({ app });
-    await new Promise((r) => app.listen({ port: 4000 }, r));
+    await new Promise((r) => app.listen({ port:process.env.PORT|| 4000 }, r));
     console.log(
       `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
     );
