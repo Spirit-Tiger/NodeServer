8137d47ac19fab6d9b94a2d42feb115ce428eb3a
Spirit-Tiger <sprttiger@gmail.com>
2022-08-26 15:48:37 +0300

    cors

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ mongoose
     const app = express();
 
     app.use(graphqlUploadExpress());
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", '*');
+      res.header("Access-Control-Allow-Credentials", true);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+      next();
+  });
 
     server.applyMiddleware({ app });
     await new Promise((r) => app.listen({ port: process.env.PORT || 4000 }, r));
