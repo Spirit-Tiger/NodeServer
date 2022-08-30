ffc3f05ab23e6b6cad89d740feeb49a45a1a3cd5
Spirit-Tiger <sprttiger@gmail.com>
2022-08-30 21:05:14 +0300

    fix ferify link

--- utils/emailValidation.js
+++ utils/emailValidation.js
@@ -21,7 +21,7 @@ export const sendConfirmationEmail = (name, email, confirmationCode) => {
       html: `<h1>Email Confirmation</h1>
             <h2>Hello ${name}</h2>
             <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
-            <a href=http://localhost:3000/confirm/${confirmationCode}> Click here</a>
+            <a href=https://iqo-1.vercel.app//confirm/${confirmationCode}> Click here</a>
             </div>`,
     })
     .catch((err) => console.log(err));
